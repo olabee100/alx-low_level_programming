d942e5d4fe36703ff2e4bced6e6fbca3371a7fa4
olabee100 <olabayoantonio@gmail.com>
2022-09-08 11:57:25 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
- * Return 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
