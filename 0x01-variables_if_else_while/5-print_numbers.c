5a6f2b16f67cd27980e92d261a2b0c0ab2007572
olabee100 <olabayoantonio@gmail.com>
2022-09-09 18:13:05 -0700

    cool

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while(i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+
+	return (0);
+}
