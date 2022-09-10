db5d2e086730f17f50f85d3622e519f446a42e83
olabee100 <olabayoantonio@gmail.com>
2022-09-09 17:44:35 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,18 +8,23 @@
 
 int main(void)
 {
-	int a, b;
+	int a, j;
+
+	a = 97;
+	j = 65;
 
 	while (a < 123)
 	{
 		putchar(a);
 		a++;
 	}
-	while (b < 91)
+
+	while (j < 91)
 	{
-		putchar(b);
-		b++;
+		putchar(j);
+		j++;
 	}
+
 	putchar(10);
 
 	return (0);
