ed4d55d868bd9622723a5854cc439a2664a5576b
olabee100 <olabayoantonio@gmail.com>
2022-09-09 17:51:55 -0700

    ok

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -Entry pont
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
