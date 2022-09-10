adcb2f1e8f47be0039c559d4730a4f776e8d7566
olabee100 <olabayoantonio@gmail.com>
2022-09-09 17:36:00 -0700

    moving

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b < 91)
+	{
+		putchar(b);
+		b++;
+	}
+	putchar(10);
+
+	return (0);
+}
