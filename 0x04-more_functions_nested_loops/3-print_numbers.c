6b1771b93b607833888c97b1c227bb60ccae1164
olabee100 <olabayoantonio@gmail.com>
2022-09-15 08:04:26 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  print_numbers - function that prints numbers
+ */
+
+void print_numbers(void);
+{
+	char c = '0';
+
+	while (c <= '9';)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
