0c992b3b4ef397f5f3f63841cb472f1002b94b13
olabee100 <olabayoantonio@gmail.com>
2022-09-15 13:52:40 -0700

    ok

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,9 @@ void more_numbers(void)
 	{
 		for (c = 0; c <= 14; c++)
 		{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
