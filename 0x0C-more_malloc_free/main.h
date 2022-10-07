a39a88288f893daaf59bbbd94e84722d84dd7fc8
olabee100 <olabayoantonio@gmail.com>
2022-10-06 20:58:36 -0700

    ok

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,8 +1,17 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-more_malloc_free directory.
+ */
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-int main(int argc, char *argv[])
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif
