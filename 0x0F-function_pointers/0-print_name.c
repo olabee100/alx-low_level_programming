51f03511cb13f059605844a2b100891dd067bcc5
olabee100 <olabayoantonio@gmail.com>
2022-10-12 14:50:38 -0700

    ok

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/**
+ * print_name - a function that prints a name.
+ * @name: Character Pointer
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+ *
+ * author - Antonio Olabayo
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
